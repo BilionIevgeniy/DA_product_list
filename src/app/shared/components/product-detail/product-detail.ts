ed93c46e4936e79ae8b780b17a3724d592c7adc9
BilionIevgeniy <bilionjenia1111@gmail.com>
2026-05-14 17:45:41 +0200

    added form

--- src/app/shared/components/product-detail/product-detail.ts
+++ src/app/shared/components/product-detail/product-detail.ts
@@ -6,4 +6,13 @@ import { Component } from '@angular/core';
   templateUrl: './product-detail.html',
   styleUrl: './product-detail.scss',
 })
-export class ProductDetail {}
+export class ProductDetail {
+  detail = {
+    name: 'Gaming Maus',
+    description:
+      'Eine ergonomische Gaming-Maus mit hoher Präzision und einstellbare DPI. Ideal für FPS- und MOBA-Spiele, bietet sie eine langlebige Bauweise und komfortableSeitentasten für schnelles Reagieren.',
+    specs: 'dpi: 6400, cable length: 1.8m, color: Schwarz',
+    stock: 120,
+    price: 2500000,
+  };
+}
