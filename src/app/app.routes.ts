d62156f4f79ca098269a3cda22e0625cc36d5c7e
BilionIevgeniy <bilionjenia1111@gmail.com>
2026-05-15 13:25:26 +0200

    modified header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,35 @@
 import { Routes } from '@angular/router';
 import { ProductDetail } from './shared/components/product-detail/product-detail';
 import { ProductList } from './shared/components/product-list/product-list';
+import { ProductForm } from './shared/components/product-form/product-form';
 
 export const routes: Routes = [
   {
     path: '',
     component: ProductList,
+    data: {
+      btnText: 'Neues Produkt',
+      btnPath: 'form',
+      path: '',
+    },
   },
   {
     path: 'detail/:name',
     component: ProductDetail,
+    data: {
+      btnText: 'Zurück zur Liste',
+      btnPath: '',
+      path: 'detail',
+    },
+  },
+  {
+    path: 'form',
+    component: ProductForm,
+    data: {
+      btnText: 'Abbrechen',
+      btnPath: '',
+      path: 'form',
+    },
   },
   // Fallback for unknown routes
   {
--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,36 +1,41 @@
 import { Component, computed, effect, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { NavigationEnd, Router } from '@angular/router';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
 import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   standalone: true,
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
   private router = inject(Router);
-  public currentUrl = toSignal(
-    this.router.events.pipe(
-      filter((e: any): e is NavigationEnd => e instanceof NavigationEnd),
-      map((e: NavigationEnd) => e.urlAfterRedirects),
-    ),
-    { initialValue: this.router.url },
-  );
 
-  public path = computed(() => {
-    return this.currentUrl().includes('detail') ? '' : 'detail';
+  public btnPath = computed(() => {
+    return this.activeRouteData()['btnPath'] ?? '';
   });
 
   public btnText = computed(() => {
-    return this.currentUrl().includes('detail') ? 'zurück zur Liste' : 'Neues Produkt';
+    return this.activeRouteData()['btnText'] ?? 'Neues Produkt';
   });
 
-  constructor() {
-    effect(() => {
-      console.log('URL изменился на:', this.currentUrl());
-    });
-  }
+  public path = computed(() => {
+    return this.activeRouteData()['path'] ?? '';
+  });
+
+  private activeRouteData = toSignal(
+    this.router.events.pipe(
+      filter((e: any): e is NavigationEnd => e instanceof NavigationEnd),
+      map(() => {
+        let currentRoute = this.router.routerState.root;
+        while (currentRoute.firstChild) {
+          currentRoute = currentRoute.firstChild;
+        }
+        return currentRoute.snapshot.data;
+      }),
+    ),
+    { initialValue: {} as Record<string, any> },
+  );
 }
